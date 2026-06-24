73b3b2bbc78b35a38a45ccd22fac04ae5fb8ed5e
Xussko <nkhus23@freeuni.edu.ge>
2026-06-25 01:52:39 +0400

    add username and firebaseUid lookup to UserRepository

--- backend/src/main/java/com/fyo/repository/UserRepository.java
+++ backend/src/main/java/com/fyo/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.fyo.repository;
 
 import com.fyo.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+
     Optional<User> findByFirebaseUid(String firebaseUid);
 }
